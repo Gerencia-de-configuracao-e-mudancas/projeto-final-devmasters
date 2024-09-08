cfb22a68206db508bce7162b8b30f469c1e904bd
Diego Sousa <diego7lts7@gmail.com>
2024-09-08 15:24:41 -0300

    ajustando caminho da imagem loading.

--- js/functions/loading/Loading.js
+++ js/functions/loading/Loading.js
@@ -2,7 +2,8 @@ function Loading(conteiner) {
   conteiner.innerHTML = "";
   let imgLoading = document.createElement("img");
   imgLoading.className = "imgLoading";
-  imgLoading.src = "./assets/img/Bean Eater@1x-1.0s-200px-200px.svg";
+  imgLoading.src =
+    "https://github.com/Gerencia-de-configuracao-e-mudancas/projeto-final-devmasters/blob/featue/requests-renderGenre/assets/img/Bean%20Eater%401x-1.0s-200px-200px.svg";
   conteiner.appendChild(imgLoading);
 }
 
