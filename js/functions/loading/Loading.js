7948a1b2ea7140b6a051ae4b9846f86395699eb6
Diego Sousa <diego7lts7@gmail.com>
2024-09-08 15:19:17 -0300

    ajustando src da imagem loading.

--- js/functions/loading/Loading.js
+++ js/functions/loading/Loading.js
@@ -2,7 +2,7 @@ function Loading(conteiner) {
   conteiner.innerHTML = "";
   let imgLoading = document.createElement("img");
   imgLoading.className = "imgLoading";
-  imgLoading.src = "../../../assets/img/Bean Eater@1x-1.0s-200px-200px.svg";
+  imgLoading.src = "./assets/img/Bean Eater@1x-1.0s-200px-200px.svg";
   conteiner.appendChild(imgLoading);
 }
 
