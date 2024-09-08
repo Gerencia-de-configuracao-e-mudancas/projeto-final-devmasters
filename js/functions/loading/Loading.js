fd4a3cb15a8416afa9b39bc0075345317685427e
Diego Sousa <diego7lts7@gmail.com>
2024-09-08 15:31:17 -0300

    alterando imagem para pasta loading.

--- js/functions/loading/Loading.js
+++ js/functions/loading/Loading.js
@@ -2,8 +2,7 @@ function Loading(conteiner) {
   conteiner.innerHTML = "";
   let imgLoading = document.createElement("img");
   imgLoading.className = "imgLoading";
-  imgLoading.src =
-    "https://github.com/Gerencia-de-configuracao-e-mudancas/projeto-final-devmasters/blob/featue/requests-renderGenre/assets/img/Bean%20Eater%401x-1.0s-200px-200px.svg";
+  imgLoading.src = "./Bean Eater@1x-1.0s-200px-200px.svg";
   conteiner.appendChild(imgLoading);
 }
 
