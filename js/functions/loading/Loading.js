f58e00f7504206fc695576fc01897f51cd210772
Diego Sousa <diego7lts7@gmail.com>
2024-09-08 15:45:52 -0300

    colocando a imagem loading nos assets.

--- js/functions/loading/Loading.js
+++ js/functions/loading/Loading.js
@@ -2,7 +2,8 @@ function Loading(conteiner) {
   conteiner.innerHTML = "";
   let imgLoading = document.createElement("img");
   imgLoading.className = "imgLoading";
-  imgLoading.src = "https://gerencia-de-configuracao-e-mudancas.github.io/projeto-final-devmasters/js/functions/loading/Bean%20Eater@1x-1.0s-200px-200px.svg";
+  imgLoading.src =
+    "https://gerencia-de-configuracao-e-mudancas.github.io/projeto-final-devmasters/assets/img/Bean%20Eater@1x-1.0s-200px-200px.svg";
   conteiner.appendChild(imgLoading);
 }
 
